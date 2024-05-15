2698bb9a38047428ebc74f0be5cfd728344e3a74
duzykarp15kg <164787602+duzykarp15kg@users.noreply.github.com>
2024-05-15 12:49:12 +0200

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,18 +1,19 @@
 ﻿#include "heap.h"
-#include <algorithm>  // dla std::swap
+
 
 void Heap::insert(Element elem) {
     data.pushBack(elem);
     int i = data.getSize() - 1;
     while (i != 0 && data[parent(i)].key < data[i].key) {
-        std::swap(data[i], data[parent(i)]);
+        swap(data[i], data[parent(i)]);
         i = parent(i);
     }
 }
 
 Element Heap::extractMax() {
-    if (data.isEmpty())
-        throw std::runtime_error("Heap is empty");
+    if (data.isEmpty()) {
+        cout << "";
+    }
 
     Element max = data[0];
     data[0] = data[data.getSize() - 1];
@@ -21,11 +22,15 @@ Element Heap::extractMax() {
     return max;
 }
 
-Element Heap::findMax()  {
+Element Heap::findMax() {
     if (data.isEmpty())
-        throw std::runtime_error("Heap is empty");
- 
-    return data[0]; 
+        throw runtime_error("Kopiec jest pusty");
+
+    return data[0];
+}
+void Heap::displayMax() {
+    const Element& maxElement = data[0];
+    cout << maxElement.value <<"(" << maxElement.key <<")"<<endl;
 }
 
 void Heap::heapify(int index) {
@@ -37,22 +42,19 @@ void Heap::heapify(int index) {
     if (r < data.getSize() && data[r].key > data[largest].key)
         largest = r;
     if (largest != index) {
-        std::swap(data[index], data[largest]);
+        swap(data[index], data[largest]);
         heapify(largest);
     }
 }
 
 void Heap::bubbleUp(int index) {
     while (index != 0 && data[parent(index)].key < data[index].key) {
-        std::swap(data[index], data[parent(index)]);
+        swap(data[index], data[parent(index)]);
         index = parent(index);
     }
 }
 
 void Heap::modifyKey(int index, int newKey) {
-    if (index < 0 || index >= data.getSize())
-        throw std::runtime_error("Index out of bounds");
-
     int oldKey = data[index].key;
     data[index].key = newKey;
     if (newKey > oldKey) {
@@ -62,7 +64,19 @@ void Heap::modifyKey(int index, int newKey) {
         heapify(index);
     }
 }
-
+void Heap::displaySize() {
+    cout << "Rozmiar kopca: " << returnSize() << endl;
+}
 int Heap::returnSize() {
-    return data.getSize();  
+    return data.getSize();
+}
+void Heap::clear() {
+    data.setSize(0);
+}
+void Heap::display() {
+    cout << "Zawartość kopca: ";
+    for (int i = 0; i < data.getSize(); ++i) {
+        cout << data[i].value << "("<<data[i].key<<")"<< " ";
+    }
+    cout << endl;
 }
