92acd55b6d7f6719fea4dc948ff60d31f914057b
duzykarp15kg <164787602+duzykarp15kg@users.noreply.github.com>
2024-05-15 12:51:54 +0200

    Update DynamicArrayPriorityQueue.h

--- DynamicArrayPriorityQueue.h
+++ DynamicArrayPriorityQueue.h
@@ -1,9 +1,18 @@
-#pragma once
 #ifndef DYNAMIC_ARRAY_PRIORITY_QUEUE_H
 #define DYNAMIC_ARRAY_PRIORITY_QUEUE_H
 
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <conio.h>
 #include <stdexcept>
 #include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 struct ElementDynamicArray {
     int key;
@@ -21,7 +30,7 @@ public:
         elements = new ElementDynamicArray[capacity];
     }
     ~DynamicArrayPQ() {
-        delete[] elements;
+        clear();
     }
 
     void insert(ElementDynamicArray elem);
@@ -32,6 +41,9 @@ public:
     bool isEmpty() const { return size == 0; }
     int getSize() const { return size; }
     void ensureCapacity();
+    void displayinsert(ElementDynamicArray elem);
+    void displayMax();
+    void displaySize();
+    void clear();
 };
 
-#endif
