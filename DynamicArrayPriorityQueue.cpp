09999cc5a8562c56f162c8d5b06bb80f788ba35f
duzykarp15kg <164787602+duzykarp15kg@users.noreply.github.com>
2024-05-15 18:48:50 +0200

    Update DynamicArrayPriorityQueue.cpp

--- DynamicArrayPriorityQueue.cpp
+++ DynamicArrayPriorityQueue.cpp
@@ -1,6 +1,6 @@
 #include "DynamicArrayPriorityQueue.h"
 
-
+//metoda podwaja rozmiar tablicy
 void DynamicArrayPQ::ensureCapacity() {
     if (size >= capacity) {
         capacity *= 2;
@@ -12,7 +12,7 @@ void DynamicArrayPQ::ensureCapacity() {
         elements = newElements;
     }
 }
-
+//metoda dodaje nowy element do tablicy po czym wykonuje sortowanie malejące według klucza
 void DynamicArrayPQ::insert(ElementDynamicArray elem) {
     ensureCapacity();
     elements[size++] = elem;
@@ -20,16 +20,17 @@ void DynamicArrayPQ::insert(ElementDynamicArray elem) {
         return a.key > b.key;
         });
 }
+//metoda wyswietla wszystkie elementy kolejki
 void DynamicArrayPQ::displayinsert(ElementDynamicArray elem) {
     for (int i = 0; i < size; ++i) {
         cout << elements[i].value << "(" << elements[i].key << ")" << " ";
     }
     cout << endl;
 }
-
+//Metoda usuwa i zwraca pierwszy element kolejki
 ElementDynamicArray DynamicArrayPQ::extractMax() {
     if (isEmpty()) {
-        throw runtime_error("kolejka jest pusta");
+        cout<<"kolejka jest pusta";
     }
     ElementDynamicArray maxElement = elements[0];
     for (int i = 1; i < size; i++) {
@@ -38,18 +39,19 @@ ElementDynamicArray DynamicArrayPQ::extractMax() {
     size--;
     return maxElement;
 }
-
+//metoda zwraca element o najwyższym priorytecie
 ElementDynamicArray DynamicArrayPQ::findMax() {
     if (isEmpty()) {
-        throw runtime_error("kolejka jest pusta");
+        cout<<"kolejka jest pusta";
     }
     return elements[0];
 }
+//metoda wyświetla ten element
 void DynamicArrayPQ::displayMax() {
     const ElementDynamicArray& maxElement = elements[0];
     cout << maxElement.value << "(" << maxElement.key << ")" << endl;
 }
-
+//metoda zmienia klucz elementu o podanym indeksie
 void DynamicArrayPQ::modifyKey(int index, int newKey) {
     if (index < 0 || index >= size) {
         throw runtime_error("index poza zakresem");
@@ -59,13 +61,15 @@ void DynamicArrayPQ::modifyKey(int index, int newKey) {
         return a.key > b.key;
         });
 }
-
+//metoda zwraca rozmiar kolejki
 int DynamicArrayPQ::returnSize() {
     return size;
 }
+//metoda wyświetla rozmiar kolejki
 void DynamicArrayPQ::displaySize() {
     cout << "Rozmiar kolejki: " << returnSize() << endl;
 }
+//metoda usuwa wszystkie elementy z kolejki i ustawia jej rozmiar na 0
 void DynamicArrayPQ::clear() {
     if (elements != nullptr) {
         delete[] elements;
